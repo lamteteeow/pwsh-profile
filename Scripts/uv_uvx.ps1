c7b48932ed75dd8d796538f2a77f28124d442ad2
Lam Tran <lamteteeow@gmail.com>
2024-12-30 17:01:55 +0100

    corrected uv and uvx powershell completion addition

--- Scripts/uv_uvx.ps1
+++ Scripts/uv_uvx.ps1
@@ -1,3 +1,3 @@
 # Add shel completion for `uv` and `uvx`
-Add-Content -Path $PROFILE -Value '(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression'
-Add-Content -Path $PROFILE -Value '(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression'
+(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
+(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
